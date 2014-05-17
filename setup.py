525aeed49458caf1760d1c556b3a5e3094be1f49
jerry <lxb429@gmail.com>
2014-05-17 15:35:54 +0800

    change version to 0.2

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 
 setup(
     name='redis_search',
-    version='0.1',
+    version='0.2',
     packages=['redis_search'],
     include_package_data = True,
     package_data = {
-        '': ['*.dat'],
+        '': ['data/*.dat'],
     },
     author='jiedan',
     author_email='lxb429@gmail.com',
